cb1b75420af093075903406840ee85a6ae88305e
Jonathan Mines <minesja@gmail.com>
2017-09-21 11:09:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,11 +23,13 @@ function destructivelyRemoveFirstKitten(){
 }
 
 function appendKitten(name){
-
+  [kittens..., name]
+  return kittens
 }
 
 function prependKitten(name){
-
+  [name, ...kittens]
+  return kittens
 }
 
 function removeLastKitten(){
