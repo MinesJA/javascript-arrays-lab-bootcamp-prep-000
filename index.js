053c82a1cb6d73ce16295ed274205f68b94c125d
Jonathan Mines <minesja@gmail.com>
2017-09-21 11:02:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten(name){
+  push.kittens(name);
+  return kittens;
+}
+
+function destructivelyPrependKitten(name){
+  
+}
+
+function destructivelyRemoveLastKitten(){
+  
+}
+
+function destructivelyRemoveFirstKitten(){
+  
+}
+
+function appendKitten(name){
+  
+}
+
+function prependKitten(name){
+  
+}
+
+function removeLastKitten(){
+  
+}
+
+function removeFirstKitten(){
+  
+}
